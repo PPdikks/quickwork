3234598c43959abb38e975fdcd61d0201aaea726
PPdikks <105913839+PPdikks@users.noreply.github.com>
2024-12-26 10:10:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express'); // Import Express
-const app = express(); // Initialize the app
-const PORT = process.env.PORT || 3009; // Dynamic port for Vercel or local testing
-const add = require('./notes.js'); // Import notes.js file
+const express = require('express'); 
+const app = express(); 
+const PORT = process.env.PORT || 3009; 
+const add = require('./notes.js'); 
 
-// Root route ("/") - Displays a welcome message
+
 app.get('/', (req, res) => {
     setTimeout(() => {
-        res.send('Welcome to the Node.js app hosted on Vercel!'); // Wait 5 seconds to display
-    }, 5000); // 5000 ms = 5 seconds
+        res.send('Welcome to the Node.js app hosted on Vercel!'); 
+    }, 5000); 
 });
 
-// A second route ("/hey") - Just an example route
+
 app.get('/hey', (req, res) => {
     res.send('Hello bachalog!');
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`The server is listening on PORT ${PORT}`);
 });
